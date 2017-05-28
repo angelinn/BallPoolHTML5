b3bae5cfa53ef0d75c5828f5099319d36aa35d92
betrakiss <angelin.nedelchev@outlook.com>
2017-05-28 21:18:13 +0300

    Successfully moving a ball with mouse drag.

---
+++ src/pool.js
@@ -0,0 +1,71 @@
+(function() {
+  var shooting = false;
+  var shot = false;
+   function  drawScreen () {  // правоъгълното поле
+      context.fillStyle = '#6fa';
+      context.fillRect(0, 0, theCanvas.width, theCanvas.height);
+      //вътрешното поле 
+      context.strokeStyle = '#000000';
+      context.strokeRect(1,  1, theCanvas.width-2, theCanvas.height-2);
+      //топчето      
+      ball.x += xunits;
+      ball.y += yunits;
+      context.fillStyle = "#77e";  
+      context.beginPath();
+      context.arc(ball.x,ball.y,17,0,Math.PI*2,true); //радиус на топчето
+      context.closePath();
+      context.fill();
+
+      if (ball.x > theCanvas.width || ball.x < 0 ) {
+         ball.angle = 180 - ball.angle;
+      } else if (ball.y > theCanvas.height || ball.y < 0) {
+         ball.angle = 360 - ball.angle;
+      }
+
+      updateBall();
+
+   }
+
+   function updateBall() {
+	  if (shot) {
+      xunits = Math.cos(ball.angle) * speed;
+      yunits = Math.sin(ball.angle)  * speed;
+      shot = false;
+	  }
+   }
+
+   theCanvas = document.getElementById("canvas");
+   context = theCanvas.getContext("2d");
+   
+   var speed = 1; // непроменлива скорост
+   var angle = 0; //началният му ъгъл
+   var radians = 0;
+   var xunits = 0;
+   var yunits = 0;
+   var ball = {x: theCanvas.width / 2, y: theCanvas.height / 2, r:  17};
+   updateBall();
+
+   function startMovement(event) {
+if (event.x <= (ball.x + ball.r) && event.y <= (ball.y + ball.r))
+	 shooting = true;
+   }
+
+   function stopMovement(event) {
+    console.log(event)
+if (shooting){
+			var diffX = event.x - ball.x;
+			var diffY = event.y - ball.y;
+
+        var rad = Math.atan2(diffY, diffX)
+        ball.angle = rad;
+        shooting = false
+        shot = true
+		}
+   }
+
+   setInterval(drawScreen, 3);
+	theCanvas.addEventListener('mousedown', startMovement);
+	theCanvas.addEventListener('mouseup', stopMovement);	
+    theCanvas.addEventListener('touchstart', startMovement);
+	theCanvas.addEventListener('touchend', stopMovement);
+})();
