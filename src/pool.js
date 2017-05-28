028d988b7c471138ed1560a6f9e06e66a8d29cf0
betrakiss <angelin.nedelchev@outlook.com>
2017-05-29 01:29:56 +0300

    Add handle collision algorithm.

--- src/pool.js
+++ src/pool.js
@@ -31,11 +31,11 @@ function Ball(x, y) {
     }
 
     this.update = function () {
-        if (this.x > CANVAS_WIDTH - 20 || this.x < 20) {
+        if (this.x > CANVAS_WIDTH - 75 || this.x < 75) {
             this.angle = 180 - this.angle;
             this.updateUnits()
         }
-        else if (this.y > CANVAS_HEIGHT - 20 || this.y < 20) {
+        else if (this.y > CANVAS_HEIGHT - 75 || this.y < 75) {
             this.angle = 360 - this.angle;
             this.updateUnits()
         }
@@ -55,6 +55,8 @@ function Engine() {
     var _self = this;
     
     this.theCanvas = document.getElementById("canvas");
+    this.theCanvas.width = window.innerWidth - 20;
+    this.theCanvas.height = window.innerHeight - 20;
 
     CANVAS_HEIGHT = this.theCanvas.height;
     CANVAS_WIDTH = this.theCanvas.width;
@@ -71,7 +73,7 @@ function Engine() {
         this.context.fillRect(0, 0, this.theCanvas.width, this.theCanvas.height);
         
         this.context.strokeStyle = '#663300';
-        this.context.lineWidth = 15;
+        this.context.lineWidth = 100;
         this.context.strokeRect(1, 1, this.theCanvas.width - 2, this.theCanvas.height - 2);
     }
 
@@ -92,17 +94,38 @@ function Engine() {
     this.detectCollisions = function() {
         for (var i = 0; i < this.balls.length; ++i) {
             for (var j = i + 1; j < this.balls.length; ++j) {
-                if (this.balls[i].collidesWith(this.balls[j].x, this.balls[j].y, this.balls[j].r))
+                if (this.balls[i].collidesWith(this.balls[j].x, this.balls[j].y, this.balls[j].r)) {
                     console.log('COLLISON')
+                    this.handleCollision(this.balls[i], this.balls[j]);
+                }
             }
         }
+    }
+
+    this.handleCollision = function(one, other) {
+        var midpointx = (one.x + other.x) / 2;
+        var midpointy = (one.y + other.y) / 2;
+
+        var onex  =  midpointx + one.r * (one.x - other.x) / 1;
+        var oney = midpointy + one.r * (one.y - other.y) / 1;
+        var otherx = midpointx + other.r * (other.x - one.x) / 1;
+        var othery = midpointy + other.r * (other.y - one. y) / 1;
+
+        var onerad = Math.atan2(oney, onex)
+
+        one.angle = onerad * (180 / Math.PI)
+        var otherrad = Math.atan2(othery, otherx);
+        other.angle = otherrad * (180 / Math.PI)
+
+        one.updateUnits()
+        other.updateUnits()
     }
 
     this.drawBalls = function() {
         this.context.fillStyle = "#FFF";
         this.context.beginPath();
 
-        this.context.arc(this.cue.x, this.cue.y, 17, 0, Math.PI * 2, true); //радиус на топчето
+        this.context.arc(this.cue.x, this.cue.y, 30, 0, Math.PI * 2, true); //радиус на топчето
 
         this.context.closePath();
         this.context.fill();
@@ -111,7 +134,7 @@ function Engine() {
         this.context.beginPath();
 
         for (var i = 1; i < this.balls.length; ++i)
-            this.context.arc(this.balls[i].x, this.balls[i].y, 17, 0, Math.PI * 2, true);
+            this.context.arc(this.balls[i].x, this.balls[i].y, 30, 0, Math.PI * 2, true);
             
         this.context.closePath();
         this.context.fill();
@@ -135,16 +158,18 @@ function Engine() {
         }
     });
     this.theCanvas.addEventListener('touchstart', function (event) {
+        
         if (!_self.cue.collidesWith(event.touches[0].pageX, event.touches[0].pageY))
             _self.shooting = true;
     });
     this.theCanvas.addEventListener('touchmove', function (event) {
+        event.preventDefault();
         _self.lastMove = event;
     });
     this.theCanvas.addEventListener('touchend', function (event) {
         if (_self.shooting) {
-            var diffX = lastMove.touches[0].pageX - _self.cue.x;
-            var diffY = lastMove.touches[0].pageY - _self.cue.y;
+            var diffX = _self.lastMove.touches[0].pageX - _self.cue.x;
+            var diffY = _self.lastMove.touches[0].pageY - _self.cue.y;
 
             var rad = Math.atan2(diffY, diffX)
             _self.cue.angle = rad * (180 / Math.PI)
